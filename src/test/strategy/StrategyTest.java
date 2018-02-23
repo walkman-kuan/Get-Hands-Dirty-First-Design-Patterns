df2f757c4a67f7ac5c1f78b1ce3491e6784d9c1e
Kuan Lin <linkuan_ant@hotmail.com>
2018-02-22 23:34:12 -0500

    Strategy Pattern
    * Test Strategy Pattern

---
+++ src/test/strategy/StrategyTest.java
@@ -0,0 +1,21 @@
+package test.strategy;
+
+import main.strategy.behaviors.FlyNoWay;
+import main.strategy.behaviors.FlyWithWings;
+import main.strategy.behaviors.Quack;
+import main.strategy.models.Duck;
+import main.strategy.models.MallardDuck;
+
+public class StrategyTest {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck(new FlyNoWay(), new Quack());
+        mallard.display();
+        mallard.performFly();
+        mallard.performQuack();
+
+        // Change behavior dynamically at runtime
+        System.out.println("Human being modify my gene! Suddenly, I can");
+        mallard.setFlyBehavior(new FlyWithWings());
+        mallard.performFly();
+    }
+}
