7bcba5a1dace15e145fff3a6d0cfad2609d682e8
Kuan Lin <linkuan_ant@hotmail.com>
2018-02-22 23:33:56 -0500

    Strategy Pattern
    * Add Duck and its subclasses

---
+++ src/main/strategy/models/MallardDuck.java
@@ -0,0 +1,17 @@
+package main.strategy.models;
+
+import main.strategy.behaviors.FlyBehavior;
+import main.strategy.behaviors.QuackBehavior;
+
+public class MallardDuck extends Duck {
+
+    public MallardDuck(FlyBehavior flyBehavior, QuackBehavior quackBehavior) {
+        this.flyBehavior = flyBehavior;
+        this.quackBehavior = quackBehavior;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Looks like a mallard!");
+    }
+}
---
+++ src/main/strategy/models/RubberDuck.java
@@ -0,0 +1,8 @@
+package main.strategy.models;
+
+public class RubberDuck extends Duck {
+    @Override
+    public void display() {
+        System.out.println("Looks like a rubberduck!");
+    }
+}
