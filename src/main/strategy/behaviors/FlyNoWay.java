8edf94b13994fb4e3bdf7881daf8a0628242deb0
Kuan Lin <linkuan_ant@hotmail.com>
2019-12-15 11:33:04 -0500

    Update

--- src/main/strategy/behaviors/FlyNoWay.java
+++ src/main/strategy/behaviors/FlyNoWay.java
@@ -1,18 +1,8 @@
 package main.strategy.behaviors;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 public class FlyNoWay implements FlyBehavior {
-  LinkedHashSet lhs;
-  HashSet hs;
-  Queue<Integer> minHeap = new PriorityQueue<>();
-
   @Override
   public void fly() {
-    minHeap.offer(1);
     System.out.println("I don't fly...");
   }
 }
